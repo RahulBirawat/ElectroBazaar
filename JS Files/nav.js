ae805368eeedb0ee119db0d8318125a18f316f06
RahulBirawat <135158644+RahulBirawat@users.noreply.github.com>
2024-01-04 10:16:02 +0530

    Add files via upload

---
+++ JS Files/nav.js
@@ -0,0 +1,26 @@
+let menu_btn = document.querySelector(".menu_bar img");
+let nav_bar = document.querySelector(".nav_bar");
+let nav_headings = document.querySelector("nav");
+let nav_btns = document.querySelector(".btns");
+let dummy = document.querySelector(".dummy");
+let logo = document.querySelector(".logo");
+menu_btn.addEventListener("click",()=>{
+    nav_bar.style.height = "100vh";
+    nav_bar.style.width = "100%";
+    nav_bar.style.backgroundColor = "white";
+    nav_bar.zIndex = "100";
+    nav_bar.style.display = "flex";
+    nav_bar.style.justifyContent = "center";
+    nav_bar.style.alignItems = "center";
+    nav_bar.style.flexDirection = "column";
+    logo.style.display = "none";
+    menu_btn.style.display = "none";
+    nav_headings.style.display = "flex";
+    nav_headings.style.justifyContent = "center";
+    nav_headings.style.alignItems = "center";
+    nav_headings.style.flexDirection = "column";
+    nav_btns.style.display = "flex";
+    nav_btns.style.justifyContent = "center";
+    nav_btns.style.alignItems = "center";
+    nav_btns.style.flexDirection = "column";
+});
