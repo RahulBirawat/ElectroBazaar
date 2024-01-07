ab9b06ded41aec34eb74f91b46a2b0f9c6ab59b1
RahulBirawat <135158644+RahulBirawat@users.noreply.github.com>
2024-01-07 20:44:56 +0530

    Update display.js

--- JS Files/display.js
+++ JS Files/display.js
@@ -7,19 +7,19 @@ let comp_div = document.querySelector("#computer");
 let cctv_div = document.querySelector("#cctv");
 
 comp_btn.addEventListener("click",()=>{
-    Changeimage.src = "../Images/computeraccesories.jpg";
+    Changeimage.src = "./Images/computeraccesories.jpg";
     cctv_div.style.display = "none";
     mobile_div.style.display = "none";
     comp_div.style.display = "block";
 });
 mob_btn.addEventListener("click",()=>{
-    Changeimage.src = "../Images/mobile.jpg";
+    Changeimage.src = "./Images/mobile.jpg";
     cctv_div.style.display = "none";
     mobile_div.style.display = "block";
     comp_div.style.display = "none";
 });
 cctv_btn.addEventListener("click",()=>{
-    Changeimage.src = "../Images/cctv-accessories.jpg";
+    Changeimage.src = "./Images/cctv-accessories.jpg";
     cctv_div.style.display = "block";
     mobile_div.style.display = "none";
     comp_div.style.display = "none";
